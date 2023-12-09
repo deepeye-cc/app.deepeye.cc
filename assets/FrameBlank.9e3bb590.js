ef7b5a63fc09a6887cca0bd4bde33d8e15999b19
mbp13 <mbp13@192.168.0.107>
2023-12-09 13:36:04 +0800

    update

---
+++ assets/FrameBlank.9e3bb590.js
@@ -0,0 +1,1 @@
+import{_ as e,d as a,g as n,c as r}from"./index.58167fef.js";const o=a({name:"FrameBlank"});function s(t,p,c,m,_,d){return n(),r("div")}var f=e(o,[["render",s],["__file","/Users/mbp13/git/deepeye/app-deepeye/src/layouts/components/FrameBlank.vue"]]);export{f as default};
