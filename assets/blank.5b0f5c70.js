8f517564f8a04336da3e471a836244117a39f94d
Leafan.Chan <17399496+leafan@users.noreply.github.com>
2023-12-19 20:43:29 +0800

    Add files via upload

---
+++ assets/blank.5b0f5c70.js
@@ -0,0 +1,1 @@
+import{_,c as t,g as r,h as s,e as a}from"./index.88332f36.js";const o={},n={class:"tdesign-wrapper"};function c(p,d){const e=t("router-view");return a(),r("div",n,[s(e)])}var i=_(o,[["render",c],["__scopeId","data-v-082ee776"],["__file","/Users/mbp13/git/deepeye/app-deepeye/src/layouts/blank.vue"]]);export{i as default};
