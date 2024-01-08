5500acdc4f25179ad059bbfb16772696fa748680
Leafan.Chan <17399496+leafan@users.noreply.github.com>
2024-01-08 15:35:26 +0800

    Add files via upload

---
+++ assets/FrameBlank.dceaae95.js
@@ -0,0 +1,1 @@
+import{_ as e,d as a,e as n,g as r}from"./index.7c53035d.js";const o=a({name:"FrameBlank"});function s(t,p,c,m,_,d){return n(),r("div")}var f=e(o,[["render",s],["__file","/Users/mbp13/git/deepeye/app-deepeye/src/layouts/components/FrameBlank.vue"]]);export{f as default};
